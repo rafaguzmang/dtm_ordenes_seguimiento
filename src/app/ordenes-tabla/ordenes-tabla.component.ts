fd27a2ed7ef9463171dc9c2ee4299bc4b8b36acf
rafaguzmang <55261233+rafaguzmang@users.noreply.github.com>
2025-02-10 15:44:15 -0600

    Se agrega botón para restaurar valores

--- src/app/ordenes-tabla/ordenes-tabla.component.ts
+++ src/app/ordenes-tabla/ordenes-tabla.component.ts
@@ -15,28 +15,20 @@ export class OrdenesTablaComponent implements OnInit {
   tabla: any[] = [];
   clientes: string[] = [];
   status: string[] = [];
-  diferencia:number = 0;
-  nesteo: number = 0;
-  retrazo: number = 0;
-  urgente: number = 0;
   total: number = 0;
-  ordenes: number = 0;
-  calidad: number = 0;
-  terminado: number = 0;
-  instalacion: number = 0;
-  tardia: number = 0;
-  nostatus: number = 0;
   
   constructor(
     private odoocon:OdooService,
     private datosordenes:DataService
   ){}
-
+  
+  actionRestart() {
+    this.fetchOrdenes();
+  }
   statusSearch($event: Event) {
     const statusVal = $event.target as HTMLInputElement;
     let newtabla:any[] = [];
     let valor:string = '';
-    console.log(statusVal.value);
     this.tabla.forEach(item =>{
       if(statusVal.value === 'Nesteo'){
         valor = 'aprobacion';
@@ -109,139 +101,7 @@ export class OrdenesTablaComponent implements OnInit {
     this.tabla = newtabla;
     this.clientesList();
     this.statusList();
-  }
-
-  ordenesNinguno() {
-    let newtabla:any = [];
-    this.datosordenes.getOrdenes().forEach(val =>{
-      if (val.status === false){
-        newtabla.push(val);
-      }
-    });
-    this.tabla = [];
-    this.tabla = newtabla;
-    this.clientesList();
-    this.statusList();
-  }
-
-  ordenesInstalacion() {
-    let newtabla:any = [];
-    this.datosordenes.getOrdenes().forEach(val =>{
-      if (val.status === 'instalacion'){
-        newtabla.push(val);
-      }
-    });
-    this.tabla = [];
-    this.tabla = newtabla;
-    this.clientesList();
-    this.statusList();
-  }
-
-  ordenesCalidad() {
-    let newtabla:any = [];
-    this.datosordenes.getOrdenes().forEach(val =>{
-      if (val.status === 'calidad'){
-        newtabla.push(val);
-      }
-    });
-    this.tabla = [];
-    this.tabla = newtabla;  
-    this.clientesList();
-    this.statusList();
-  }
-
-  ordenesSinFactura() {
-    let newtabla:any = [];
-    this.datosordenes.getOrdenes().forEach(val =>{
-      if (val.status === 'terminado'){
-        newtabla.push(val);
-      }
-    });
-    this.tabla = [];
-    this.tabla = newtabla;
-    this.clientesList();
-    this.statusList();
-  }
-
-  ordenesTotal() {
-    this.tabla = [];
-    this.tabla = this.datosordenes.getOrdenes();
-    this.clientesList();
-    this.statusList();
-  }
-
-  ordenesTardia() {
-    let newtabla:any = [];
-    let criterio = ['calidad','terminado','instalacion'];
-    this.datosordenes.getOrdenes().forEach(val=>{ 
-      if (((val.status === 'aprobacion' || val.status === false) && Number(new Date(val.date_rel))-4 <= Number(new Date())) && criterio.indexOf(val.status)===-1 ){
-        newtabla.push(val);
-      }
-    })    
-    this.tabla = [];
-    this.tabla = newtabla;
-    this.clientesList();
-    this.statusList();
-
-  }
-
-  ordenesUrgente() {
-    let newtabla:any = [];
-    let criterio = ['calidad','terminado','instalacion'];
-    this.datosordenes.getOrdenes().forEach(val=>{
-      if (Number(new Date()) >= (Number(new Date(val.date_rel))-4) && criterio.indexOf(val.status)===-1){        
-        newtabla.push(val);
-      }
-    })
-    this.tabla = [];
-    this.tabla = newtabla;
-    this.clientesList();
-    this.statusList();
-
-  }
-
-  ordenesRetrazo() {
-    let newtabla:any = [];
-    let criterio = ['calidad','terminado','instalacion'];
-    this.datosordenes.getOrdenes().forEach(val=>{
-      if (((new Date(val.date_rel)) < (new Date(val.create_date))) && criterio.indexOf(val.status)===-1 ){        
-        newtabla.push(val);
-      }
-    })
-    this.tabla = [];
-    this.tabla = newtabla;
-    this.clientesList();
-    this.statusList();
-    
-  }
-
-  ordenesNesteo() {    
-    let newtabla:any = [];
-    this.datosordenes.getOrdenes().forEach(item =>{
-        if(item.status==='aprobacion'){
-          newtabla.push(item);
-        }
-    })
-    this.tabla = [];
-    this.tabla = newtabla;
-    this.clientesList();
-    this.statusList();
-  }
-
-  ordenesFiltro() {
-    let newtabla:any = [];
-    let criterio = ['calidad','terminado','instalacion',false];
-    this.datosordenes.getOrdenes().forEach(item =>{
-        if(criterio.indexOf(item.status)===-1){
-          newtabla.push(item);
-        }
-    })
-    this.tabla = [];
-    this.tabla = newtabla;
-    this.clientesList();
-    this.statusList();
-
-  }
+  } 
 
   statusList() {
     let status:string[] = [];
@@ -294,12 +154,10 @@ export class OrdenesTablaComponent implements OnInit {
           // Filtra del que le faltan menos días para entregar al que          
           this.tabla = this.datosordenes.getOrdenes();
           this.tabla.sort((a,b)=> a.diferencia - b.diferencia);
-          this.total = this.tabla.length;             
-          // this.retrazo =  this.retrazoComp();
-          // this.urgente = this.urgenteComp();
-          // this.tardia = this.tardiaComp();
-          // this.clientesList();
-          // this.statusList();
+          this.total = this.tabla.length;
+          this.clientesList();
+          this.statusList();
+            
         })
     })
   }  
@@ -308,53 +166,13 @@ export class OrdenesTablaComponent implements OnInit {
     this.fetchOrdenes();
   }
 
-  contador(tipo:string):number{
-    let contador = 0;
-    this.tabla.forEach(val =>{
-      if (tipo === 'false'){
-        if(val.status === false){
-          contador++;
-        }
+ 
 
-      } else if(val.status === tipo){
-        contador++;
-      }
-    });
-    return contador;
-  }
+  
 
-  retrazoComp():number{
-    let contador = 0;
-    let criterio = ['calidad','terminado','instalacion'];
-    this.tabla.forEach(val=>{
-      if (((new Date(val.date_rel)) < (new Date(val.create_date))) && criterio.indexOf(val.status)===-1){        
-        contador++;
-      }
-    })
-    return contador;
-  }
+  
 
-  urgenteComp():number{
-    let contador = 0;
-    let criterio = ['calidad','terminado','instalacion'];
-    this.tabla.forEach(val=>{
-      if ((Number(new Date(val.date_rel))-4 <= Number(new Date())) && criterio.indexOf(val.status)===-1 ){        
-        contador++;
-      }
-    })
-    return contador;
-  }
-
-  tardiaComp():number{
-    let contador = 0;
-    let criterio = ['calidad','terminado','instalacion'];
-    this.tabla.forEach(val=>{ 
-      if (((val.status === 'aprobacion' || val.status === false) && Number(new Date(val.date_rel))-4 <= Number(new Date())) && criterio.indexOf(val.status)===-1 ){
-        contador++;
-      }
-    })    
-    return contador;
-  }
+ 
  
   
 }
