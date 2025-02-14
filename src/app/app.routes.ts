6c4603f7367669fc1ec0076d2bd7f1c9cb29543e
rafaguzmang <55261233+rafaguzmang@users.noreply.github.com>
2025-02-14 15:05:20 -0600

    Se crea tabla para el seguimiento de las ordenes de diseño

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { OrdenesTablaComponent } from './ordenes-tabla/ordenes-tabla.component';
 import { DisenoTablaComponent } from './diseno-tabla/diseno-tabla.component';
 
 export const routes: Routes = [
-    { path: 'manufactura', component: OrdenesTablaComponent },
-    { path: '', component: DisenoTablaComponent },
+    { path: '', component: OrdenesTablaComponent },
+    { path: 'diseno', component: DisenoTablaComponent },
 ];
